063cc0a345fb03ea0ea4a2768f09e674782ecb06
disasterservices <111297123+disasterservices@users.noreply.github.com>
2023-10-02 14:10:10 +0100

    try again3

--- Inc/config.h
+++ Inc/config.h
@@ -166,11 +166,8 @@
 // Extra functionality
 // #define STANDSTILL_HOLD_ENABLE          // [-] Flag to hold the position when standtill is reached. Only available and makes sense for VOLTAGE or TORQUE mode.
 #define ELECTRIC_BRAKE_ENABLE           // [-] Flag to enable electric brake and replace the motor "freewheel" with a constant braking when the input torque request is 0. Only available and makes sense for TORQUE mode.
-<<<<<<< HEAD
 #define ELECTRIC_BRAKE_MAX    400       // (0, 500) Maximum electric brake to be applied when input torque request is 0 (pedal fully released).
 #define ELECTRIC_BRAKE_THRES  150       // (0, 500) Threshold below at which the electric brake starts engaging.
-=======
->>>>>>> 8a9d7c8dc8207139f62b8dae732d602670cf2be5
 // ########################### END OF MOTOR CONTROL ########################
 
 
